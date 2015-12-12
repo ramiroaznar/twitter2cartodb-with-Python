e689d473d6ff4cc6af1d925a047bcf0f550b84fc
Ramiro Aznar <ramiroaznar@gmail.com>
2015-12-12 11:40:00 +0100

    Create tweet2csv.py

---
+++ tweet2csv.py
@@ -0,0 +1,170 @@
+'''
+Author: Ramiro Aznar
+Web: www.ramiroaznar.com
+Language: Python
+Date: December 12th 2015
+Code: print and save tweets from a certain hashtag from the past, all tweets are 
+categorized according to their sentiment, question and location if they have geo tag
+Note: to speed up the code, comment print statements
+'''
+
+#import modules
+import tweepy
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+
+import geopy
+from geopy.geocoders import Nominatim
+geolocator = Nominatim()
+
+from textblob import TextBlob
+
+#declare variables
+ckey = "CONSUMER_KEY"
+csecret = "CONSUMER_SECRET"
+atoken = "ACCESS_TOKEN"
+asecret = "ACCESS_SECRET"
+
+OAUTH_KEYS = {'consumer_key':ckey, 'consumer_secret':csecret, 'access_token_key':atoken, 'access_token_secret':asecret}
+auth = tweepy.OAuthHandler(OAUTH_KEYS['consumer_key'], OAUTH_KEYS['consumer_secret'])
+api = tweepy.API(auth)
+
+#open file
+saveFile = open('tweets.csv', 'a')
+headers = 'Time; User; Text; Sentiment; Question; Place; Latitude; Longitude;'
+saveFile.write(headers + '\n')
+
+#print tweets
+for tweet in tweepy.Cursor(api.search, q ='#tweetdatapoint').items():
+    '''
+    Parameters (*required):
+    q*: word, hashtag of interest [#hashtag]
+    geocode: returns tweets by users located within a given radius of the given latitude/longitude [37.781157,-122.398720,1km]
+    lang: language [es]
+    result_type: [mixed, popular or recent]
+    count: [15-100]
+    until: YYYY-MM-DD, 7-day limit [2015-12-5]
+    since_id: returns results with an ID greater than (that is, more recent than) the specified ID [12345]
+    '''
+
+    if tweet.coordinates:
+        print "==========="
+        #date & time
+        moment = tweet.created_at
+        print 'Date & Time: ', moment
+        #text
+        string = tweet.text.replace('|', ' ')
+        string = tweet.text.replace('\n', ' ')
+        print 'Text: ', string.encode('utf8')
+        #user
+        user = tweet.author.name.encode('utf8')
+        print 'User: ', user
+        #sentiment
+        text = TextBlob(string)
+        if text.sentiment.polarity < 0:
+            sentiment = "negative"
+        elif text.sentiment.polarity == 0:
+            sentiment = "neutral"
+        else:
+            sentiment = "positive"
+        print 'Sentiment: ', sentiment
+        #question
+        if '?' in string:
+            question = 'Yes'
+        else:
+            question = 'No'
+        print 'Question: ', question
+        #location    
+        place = geolocator.reverse(str(tweet.coordinates))
+        print 'Place: ', place
+        latitude = tweet.coordinates[0]
+        longitude = tweet.coordinates[1]
+        print 'Coords:', tweet.coordinates
+        print "==========="
+
+        #save tweets
+        saveThis = str(moment) + '; ' + string.encode('utf8') + '; ' + user + '; ' + sentiment + '; ' + question + '; ' + place + '; ' + str(latitude) + '; ' + str(longitude) + '; '
+        saveFile.write(saveThis + '\n')
+
+    if tweet.place:
+        print "==========="
+        #date & time
+        moment = tweet.created_at
+        print 'Date & Time: ', moment
+        #text
+        string = tweet.text.replace('|', ' ')
+        string = tweet.text.replace('\n', ' ')
+        print 'Text: ', string.encode('utf8')
+        #user
+        user = tweet.author.name.encode('utf8')
+        print 'User: ', user
+        #sentiment
+        text = TextBlob(string)
+        if text.sentiment.polarity < 0:
+            sentiment = "negative"
+        elif text.sentiment.polarity == 0:
+            sentiment = "neutral"
+        else:
+            sentiment = "positive"
+        print 'Sentiment: ', sentiment
+        #question
+        if '?' in string:
+            question = 'Yes'
+        else:
+            question = 'No'
+        print 'Question: ', question
+        #location
+        place = tweet.place.full_name
+        print 'Place:', place
+        location = geolocator.geocode(place)
+        latitude = location.latitude
+        longitude = location.longitude
+        print 'Coords: ', location.latitude, location.longitude
+        print "==========="
+
+        #save tweets
+        saveThis = str(moment) + '; ' + string.encode('utf8') + '; ' + user + '; ' + sentiment + '; ' + question + '; ' + place + '; ' + str(latitude) + '; ' + str(longitude) + '; '
+        saveFile.write(saveThis + '\n')
+
+    else:
+        print "==========="
+        #date & time
+        moment = tweet.created_at
+        print 'Date & Time: ', moment
+        #text
+        string = tweet.text.replace('|', ' ')
+        string = tweet.text.replace('\n', ' ')
+        print 'Text: ', string.encode('utf8')
+        #user
+        user = tweet.author.name.encode('utf8')
+        print 'User: ', user
+        #sentiment
+        text = TextBlob(string)
+        if text.sentiment.polarity < 0:
+            sentiment = "negative"
+        elif text.sentiment.polarity == 0:
+            sentiment = "neutral"
+        else:
+            sentiment = "positive"
+        print 'Sentiment: ', sentiment
+        #question
+        if '?' in string:
+            question = 'Yes'
+        else:
+            question = 'No'
+        print 'Question: ', question
+        #location
+        place = ''
+        latitude = ''
+        longitude = ''
+        print 'Place: ', place
+        print 'Coords: ', latitude, longitude
+        print "==========="
+
+        #save tweets
+        saveThis = str(moment) + '; ' + string.encode('utf8') + '; ' + user + '; ' + sentiment + '; ' + question + '; ' + place + '; ' + str(latitude) + '; ' + str(longitude) + '; '
+        saveFile.write(saveThis + '\n')
+
+#close file
+saveFile.close()
